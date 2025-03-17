715ab5215a9dc5ea530c610bda16e2f3e3b775d4
Kristian Katt <kristiankatt@gmail.com>
2025-03-17 13:46:48 +0000

    Add files via upload

---
+++ your code/solutions.sql
@@ -0,0 +1,135 @@
+-- Lab 1:
+
+
+-- Solution challenge 1:
+
+SELECT client_id
+FROM client
+WHERE district_id = 1
+ORDER BY client_id
+LIMIT 5;
+
+
+-- Solution challenge 2:
+
+SELECT client_id 
+FROM client 
+WHERE district_id = 72 
+ORDER BY client_id DESC 
+LIMIT 1;
+
+-- Solution challenge 3: 
+
+SELECT amount
+FROM loan
+ORDER BY amount ASC
+LIMIT 3;
+
+
+-- Solution challenge 4: 
+
+SELECT DISTINCT status
+FROM loan
+ORDER BY status ASC;
+
+-- Solution challenge 5: 
+
+SELECT loan_id
+FROM loan
+ORDER BY payments DESC
+LIMIT 1;
+
+-- Solution challenge 6:
+
+SELECT account_id, amount
+FROM loan
+ORDER BY account_id ASC
+LIMIT 5;
+
+-- Solution challenge 7:
+
+SELECT account_id
+FROM loan
+WHERE duration = 60
+ORDER BY amount ASC 
+LIMIT 5;
+
+-- Solution challenge 8:
+
+SELECT DISTINCT k_symbol
+FROM `order`
+ORDER BY k_symbol ASC;
+
+-- Solution challenge 9:
+
+SELECT order_id
+FROM `order`
+WHERE account_id = 34;
+
+-- Solution challenge 10:
+
+SELECT DISTINCT account_id
+FROM `order`
+WHERE order_id BETWEEN 29540 AND 29560;
+
+-- Solution challenge 11:
+
+SELECT amount
+FROM `order`
+WHERE account_to = 30067122;
+
+-- Solution challenge 12:
+
+SELECT trans_id, date, type, amount
+FROM trans
+WHERE account_id = 793
+ORDER BY date DESC
+LIMIT 10;
+
+-- Solution challenge 13:
+
+SELECT district_id, COUNT(*) AS client_count
+FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id ASC;
+
+-- Solution challenge 14:
+
+SELECT type, COUNT(*) AS card_count
+FROM card
+GROUP BY type
+ORDER BY card_count DESC;
+
+
+-- Solution challenge 15:
+
+SELECT account_id, SUM(amount) AS total_loan_amount
+FROM loan
+GROUP BY account_id
+ORDER BY total_loan_amount DESC
+LIMIT 10;
+
+-- Solution challenge 16:
+
+SELECT date, COUNT(*) AS loan_count
+FROM loan
+WHERE date < 930907
+GROUP BY date
+ORDER BY date DESC;
+
+-- Solution challenge 17:
+
+SELECT date, duration, COUNT(*) AS loan_count
+FROM loan
+WHERE date BETWEEN 971201 AND 971231
+GROUP BY date, duration
+ORDER BY date ASC, duration ASC;
+
+-- Solution 18: 
+
+SELECT account_id, type, SUM(amount) AS total_amount
+FROM trans
+WHERE account_id = 396
+GROUP BY account_id, type
+ORDER BY type ASC;
