6dad89126f04c2e9de1ba3eda3317c58365987e8
Jesús Gabriel Hernández <jesusgabrielher@gmail.com>
2025-10-09 23:39:50 +0200

    Finish lab-sql-basics V2

--- files_for_lab/solutions.sql
+++ files_for_lab/solutions.sql
@@ -1,21 +1,17 @@
 -- Query 1: Get the id values of the first 5 clients from district_id with a value equals to 1.
-SELECT account_id FROM account WHERE district_id = 1 limit 5;
-
+SELECT client_id FROM client WHERE district_id = 1 LIMIT 5;
 -- Query 2: In the client table, get an id value of the last client where the district_id equals to 72
 SELECT client_id from client where district_id = 72 ORDER BY client_id DESC LIMIT 1;
-
 -- Query 3: Get the 3 lowest amounts in the loan table.
 SELECT amount FROM loan ORDER BY amount LIMIT 3;
-
 -- Query 4: What are the possible values for status, ordered alphabetically in ascending order in the loan table?
 SELECT DISTINCT(status) from loan ORDER BY status;
-
 -- Query 5: What is the loan_id of the highest payment received in the loan table?
 SELECT loan_id FROM loan ORDER BY payments DESC LIMIT 1;
 -- Query 6: What is the loan amount of the lowest 5 account_ids in the loan table? Show the account_id and the corresponding amount
 SELECT account_id, amount FROM loan ORDER BY account_id LIMIT 5;
 --  Query 7: What are the account_ids with the lowest loan amount that have a loan duration of 60 in the loan table?
-SELECT account_id FROM loan WHERE duration = 60 ORDER BY amount ASC LIMIT 1;
+SELECT account_id FROM loan WHERE duration = 60 AND amount = (SELECT MIN(amount) FROM loan WHERE duration = 60);
 -- Query 8: What are the unique values of k_symbol in the order table?
 SELECT DISTINCT(k_symbol) FROM "order" ORDER BY k_symbol ASC;
 -- Query 9: In the order table, what are the order_ids of the client with the account_id 34?
