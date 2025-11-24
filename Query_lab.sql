a6803c4d1c7cee3aa534101e1adf68ad70763e59
Nicolas Zorrilla <ncodexz@gmail.com>
2025-11-24 10:13:39 +0100

    Add files via upload

---
+++ Query_lab.sql
@@ -0,0 +1,98 @@
+SELECT client_id
+FROM client
+WHERE district_id = 1
+ORDER BY 	client_id
+LIMIT 5;
+
+SELECT client_id 
+FROM client 
+WHERE district_id = 72 
+ORDER BY client_id DESC 
+LIMIT 1;
+
+SELECT amount 
+FROM loan 
+ORDER BY amount 
+LIMIT 3;
+
+SELECT DISTINCT status
+FROM loan
+ORDER BY status;
+
+SELECT loan_id
+FROM loan
+ORDER BY payments DESC
+LIMIT 1;
+
+SELECT account_id, amount
+FROM loan	
+ORDER BY	account_id
+LIMIT 5;
+
+SELECT account_id
+FROM loan
+WHERE duration = 60
+ORDER BY amount
+LIMIT 5;
+
+SELECT DISTINCT k_symbol
+FROM `order`;
+
+SELECT order_id
+FROM `order`
+WHERE account_id = 34;
+
+SELECT DISTINCT account_id
+FROM `order`
+WHERE order_id BETWEEN 29540 AND 29560;
+
+SELECT amount
+FROM `order`
+WHERE account_to = 30067122;
+
+SELECT trans_id, date, type, amount
+FROM trans
+WHERE account_id = 793
+ORDER BY date DESC, trans_id DESC
+LIMIT 10;
+
+SELECT district_id, COUNT(*)
+FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id;
+
+SELECT type, COUNT(*) as count
+FROM card
+GROUP BY type
+ORDER BY count DESC;
+
+SELECT account_id, SUM(amount) as total_amount
+FROM loan
+GROUP BY account_id
+ORDER BY total_amount DESC
+LIMIT 10;
+
+SELECT date, COUNT(*) as number_of_loans
+FROM loan
+WHERE date < 930907
+GROUP BY date
+ORDER BY date DESC;
+
+SELECT date, duration, COUNT(*) as number_of_loans
+FROM loan
+WHERE date BETWEEN 971201 AND 971231
+GROUP BY date, duration
+ORDER BY date, duration;
+
+SELECT account_id, type, SUM(amount) as total_amount
+FROM trans
+WHERE account_id = 396
+GROUP BY account_id, type
+ORDER BY type;
+
+SELECT account_id, type, SUM(amount) as total_amount
+FROM trans
+WHERE account_id = 396
+GROUP BY account_id, type
+ORDER BY type;
