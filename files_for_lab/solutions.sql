d3514143bdbf26f6c67a43d254c5cee08a6032ab
Aitor Vergara <aitor.vergaramartin@gmail.com>
2026-01-26 17:15:06 +0100

    fixed spelling mistakes

--- files_for_lab/solutions.sql
+++ files_for_lab/solutions.sql
@@ -1,6 +1,6 @@
 -- Query 1
 -- Get the id values of the first 5 clients from district_id with a value equals to 1.
-SELECT client_id from cliento WHERE district_id == 1 LIMIT 5;
+SELECT client_id from client WHERE district_id == 1 LIMIT 5;
 -- Query 2
 -- In the client table, get an id value of the last client where the district_id equals to 72.
 SELECT client_id from client WHERE district_id == 72 order by client_id DESC LIMIT 1;
