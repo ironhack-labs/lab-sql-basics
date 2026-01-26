52c660165724eb0c5d6cfde250ead8b52e0cefa4
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2026-01-26 17:30:45 +0100

    Add solution from 9 to 18

--- solutions.sql
+++ solutions.sql
@@ -22,3 +22,32 @@ SELECT account_id  FROM loan WHERE duration==60 ORDER BY amount ASC LIMIT 5;
 -- Query 8
 SELECT DISTINCT k_symbol FROM "order" ORDER BY k_symbol ASC;
 
+-- Query 9
+SELECT order_id FROM "order" WHERE account_id==34;
+
+-- Query 10
+SELECT DISTINCT account_id FROM "order" WHERE order_id > 29540 AND order_id <= 29560;
+
+-- Query 11
+SELECT amount FROM "order" WHERE account_to == 30067122;
+
+-- Query 12
+SELECT trans_id, date, type, amount FROM trans WHERE account_id == 793 ORDER BY date DESC LIMIT 10; 
+
+-- Query 13
+SELECT district_id, count(client_id)  FROM client WHERE district_id<10 GROUP BY district_id ORDER BY district_id ASC;
+
+-- Query 14
+SELECT type, count (type) FROM card GROUP BY type ORDER BY count(type) DESC;
+
+-- Query 15
+SELECT account_id , sum(amount) FROM loan GROUP BY account_id ORDER BY sum(amount) DESC LIMIT 10;
+
+-- Query 16
+SELECT date, count(loan_id) FROM loan WHERE date < 930907 GROUP BY date  ORDER BY date DESC;
+
+-- Query 17
+SELECT date, duration, count(loan_id) FROM loan WHERE date>=971200 AND date < 980100 GROUP BY date , duration ORDER BY date, duration ASC;
+
+-- Query 18
+SELECT account_id, type, sum(amount) AS total_amount FROM trans WHERE account_id==396 GROUP BY type ORDER BY type ASC;
