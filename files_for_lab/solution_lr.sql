f518a367d1b65e529b6487438385d0a2abe9d0b4
Laura R <lauraromerojob@gmail.com>
2026-03-02 16:56:46 +0100

    solved lab

---
+++ files_for_lab/solution_lr.sql
@@ -0,0 +1,76 @@
+SELECT account_id FROM account
+WHERE district_id==1
+LIMIT 5;
+
+SELECT client_id FROM client
+WHERE district_id==72
+ORDER BY client_id DESC
+LIMIT 1;
+
+SELECT amount FROM loan
+ORDER BY amount ASC
+LIMIT 3;
+
+SELECT DISTINCT status FROM loan
+ORDER BY status ASC;
+
+SELECT loan_id FROM loan
+ORDER BY payments DESC
+LIMIT 1;
+
+SELECT account_id, amount FROM loan
+ORDER BY account_id ASC
+LIMIT 5;
+
+SELECT account_id FROM loan
+WHERE duration==60
+ORDER BY amount ASC
+LIMIT 5;
+
+SELECT DISTINCT k_symbol FROM "order"
+WHERE TRIM(k_symbol) <> ''
+ORDER BY k_symbol ASC;
+
+SELECT order_id FROM "order"
+WHERE account_id==34;
+
+SELECT DISTINCT account_id FROM "order"
+WHERE order_id > 29540 AND order_id<29560 ;
+
+SELECT amount FROM "order"
+WHERE account_to==30067122;
+
+SELECT trans_id, "date", type, amount FROM trans
+WHERE account_id==793
+ORDER BY "date" DESC
+LIMIT 10;
+
+SELECT district_id,COUNT(*) AS client_count FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id ASC;
+
+SELECT DISTINCT type, COUNT(*) FROM card
+GROUP BY type
+ORDER BY COUNT(*) DESC;
+
+SELECT account_id, SUM(amount) FROM loan
+GROUP BY account_id
+ORDER BY SUM(amount) DESC
+LIMIT 10;
+
+SELECT "date", COUNT(*) FROM loan
+WHERE "date" < 930907
+GROUP BY "date"
+ORDER BY "date" DESC;
+
+SELECT "date", duration, COUNT(*) FROM loan
+WHERE date BETWEEN 971201 AND 971231
+GROUP BY date, duration
+ORDER BY date ASC, duration ASC;
+
+SELECT account_id, type, SUM(amount) A FROM trans
+WHERE account_id = 396
+GROUP BY account_id, type
+ORDER BY type ASC;
+
